2a45ebae5b65db180733ff88e267542c3a6cb639
ariyanabdullah <ak01859136696@gmail.com>
2022-11-10 08:54:07 +0600

    blog page first box style added

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -7,15 +7,16 @@ const Blog = () => {
     <section className="my-16">
       <div className="container mx-auto grid grid-cols-1 ">
         {/* first question */}
-        <div>
-          <h1>What is the Difference between SQL and NoSQL ?</h1>
+        <div className="py-6 px-5 rounded-lg shadow-lg">
+          <h1> 1. What is the Difference between SQL and NoSQL ?</h1>
           <p>
             {" "}
-            {"=>"} SQL databases are vertically scalable, while NoSQL databases
-            are horizontally scalable. SQL databases are table-based, while
-            NoSQL databases are document, key-value, graph, or wide-column
-            stores. SQL databases are better for multi-row transactions, while
-            NoSQL is better for unstructured data like documents or JSON.
+            <span className="font-bold text-red-500">{"=>"}</span> SQL databases
+            are vertically scalable, while NoSQL databases are horizontally
+            scalable. SQL databases are table-based, while NoSQL databases are
+            document, key-value, graph, or wide-column stores. SQL databases are
+            better for multi-row transactions, while NoSQL is better for
+            unstructured data like documents or JSON.
           </p>
         </div>
 
