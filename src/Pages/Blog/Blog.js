d0e05ba712d6e645edd335c4b78ca5cf0c604d8b
ariyanabdullah <ak01859136696@gmail.com>
2022-11-10 08:48:59 +0600

    blog page first question added

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,5 +1,3 @@
-import React, { useContext } from "react";
-
 import UseTitle from "../../Hooks/UseTitle";
 
 const Blog = () => {
@@ -7,8 +5,13 @@ const Blog = () => {
 
   return (
     <section className="my-16">
-      <div className="container mx-auto ">
-        <h1>This is Blog Page </h1>
+      <div className="container mx-auto grid grid-cols-1 ">
+        {/* first question */}
+        <div>
+          <h1>What is the Difference between SQL and NoSQL ?</h1>
+        </div>
+
+        {/* second question */}
       </div>
     </section>
   );
