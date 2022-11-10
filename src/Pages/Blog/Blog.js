353e5bd0e2578b88fe121d1fcda162927c1f345f
ariyanabdullah <ak01859136696@gmail.com>
2022-11-10 09:06:10 +0600

    third ans added

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -42,7 +42,19 @@ const Blog = () => {
 
         <div>
           <h1> 3. What is the difference between javascript and NodeJS?</h1>
-          <p></p>
+          <p>
+            <span className="font-bold text-red-500">{"=>"}</span>
+            NodeJS is a cross-platform and opensource Javascript runtime
+            environment that allows the javascript to be run on the server-side.
+            Nodejs allows Javascript code to run outside the browser. Nodejs
+            comes with a lot of modules and mostly used in web development.
+            <br />
+            <span className="font-bold text-red-500">{"=>"}</span>
+            Javascript is a Scripting language. It is mostly abbreviated as JS.
+            It can be said that Javascript is the updated version of the ECMA
+            script. Javascript is a high-level programming language that uses
+            the concept of Oops but it is based on prototype inheritance.
+          </p>
         </div>
 
         {/* fourth quesion */}
